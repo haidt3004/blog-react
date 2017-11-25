e5732b1a7c4158410d7b9229881f8b5a1e4dd73d
Lam <hqlam.bt@gmail.com>
2017-11-25 08:22:26 +0700

    Enable hot module replacement for component

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render(Component(), document.getElementById('root'));
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    console.log('Accepting the updated App component!');
+    let NewApp = require('./App').default
+    render(NewApp)
+  })
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,17 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   devServer: {
     contentBase: './dist',
     historyApiFallback: true,
     proxy: {
-      "/api": "http://localhost:3000"
+      "/api": "http://localhost:1000"
     }
   },
   devtool: 'inline-source-map',
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+  ]
 });

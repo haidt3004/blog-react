ad632233fcda64ed71169bf22a2f9cf477bed3dc
Lam <hqlam.bt@gmail.com>
2017-12-30 11:51:22 +0700

    Fix bug forget getting DB_URI from env

--- server/test/bootstrap.js
+++ server/test/bootstrap.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const path = require('path')
 const { connect, importCollection } = require('../modules/common/helpers/mongodb')
 
@@ -8,7 +9,7 @@ before('init database', function () {
 })
 
 async function initDatabase() {
-  const { con, db } = await connect('mongodb://localhost:27017/rblog')
+  const { con, db } = await connect(process.env.DB_URI)
   var p1 = importCollection(db, getDataFile('blog.posts'))
   var p2 = importCollection(db, getDataFile('common.users'))
   await Promise.all([p1, p2])

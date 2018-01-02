24230514c335127f77ee51ccb9be72127582e2dc
Lam <hqlam.bt@gmail.com>
2018-01-02 16:58:13 +0700

    Admin: fix scrolling in sidebar

--- src/mate/Sidebar.jsx
+++ src/mate/Sidebar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Drawer from 'material-ui/Drawer'
-import Menu from 'material-ui/Menu';
-import MenuItem from 'material-ui/MenuItem';
+import {List, ListItem} from 'material-ui/List'
 import FolderIcon from 'material-ui/svg-icons/file/folder'
 import LabelIcon from 'material-ui/svg-icons/action/label'
+import SettingIcon from 'material-ui/svg-icons/action/setting'
 import Divider from 'material-ui/Divider'
 import { Link } from 'react-router-dom'
 import style from './style.scss'
@@ -12,12 +12,20 @@ import style from './style.scss'
 const Sidebar = ({ visible }) => (
   <Drawer docked={true} open={visible}>
     <h1 className={style.logo}>React Blog</h1>
-    <Menu>
-      <MenuItem primaryText={<Link to="posts" className={style['menu-item']}>Posts</Link>} leftIcon={<FolderIcon />} />
-      <MenuItem primaryText={<Link to="tags" className={style['menu-item']}>Tags</Link>} leftIcon={<LabelIcon />} className={style['menu-item']} />
-      <Divider />
-      <MenuItem primaryText="Configuration" leftIcon={<FolderIcon />} />
-    </Menu>
+    <List>
+      <ListItem leftIcon={<FolderIcon />}>
+        <Link to="posts" className={style['menu-item']}>Posts</Link>
+      </ListItem>
+      <ListItem leftIcon={<LabelIcon />}>
+        <Link to="tags" className={style['menu-item']}>Tags</Link>
+      </ListItem>
+    </List>
+    <Divider />
+    <List>
+      <ListItem leftIcon={<SettingIcon />}>
+        <Link to="config" className={style['menu-item']}>Configuration</Link>
+      </ListItem>
+    </List>
   </Drawer>
 )
 

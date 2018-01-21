57b2053a10617a73085ff6903718de463c9405b4
Lam <>
2018-01-21 17:22:23 +0700

    Remove Care Interchange module

--- server/modules/ci/admin/router.js
+++
@@ -1,8 +0,0 @@
-var handlers = require('./handlers')
-var express = require('express')
-var router = express.Router()
-
-router.route('/ci/admin/session')
-  .post(handlers.login)
-
-module.exports = router
--- src/ci/reducers.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-// import { handleActions } from 'redux-actions'
-import orgReducer from './org/reducers'
-
-export default combineReducers({
-  organisation: orgReducer,
-})

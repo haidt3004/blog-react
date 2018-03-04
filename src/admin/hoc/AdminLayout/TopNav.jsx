6f2361a09ec3d6dcff139cd658f0b76f3ee404bf
Lam <hqlam.bt@gmail.com>
2018-03-04 15:31:53 +0700

    Implement logout feature

--- src/admin/hoc/AdminLayout/TopNav.jsx
+++ src/admin/hoc/AdminLayout/TopNav.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { clearIdentity } from '../../../common/actions'
 
-const TopNav = ({ username }) => (
+const TopNav = ({ username, logout }) => (
   <div className="top_nav">
     <div className="nav_menu">
       <nav>
@@ -20,19 +22,10 @@ const TopNav = ({ username }) => (
             </a>
             <ul className="dropdown-menu dropdown-usermenu pull-right">
               <li>
-                <a href="javascript:"> Profile</a>
+                <Link to={`/admin/profile`}>Profile</Link>
               </li>
               <li>
-                <a href="javascript:">
-                  <span className="badge bg-red pull-right">50%</span>
-                  <span>Settings</span>
-                </a>
-              </li>
-              <li>
-                <a href="javascript:">Help</a>
-              </li>
-              <li>
-                <a href="login.html">
+                <a role="button" onClick={logout}>
                   <i className="fa fa-sign-out pull-right"></i> Log Out</a>
               </li>
             </ul>
@@ -43,6 +36,11 @@ const TopNav = ({ username }) => (
   </div>
 )
 
-export default connect(state => ({
-  username: state.common.identity.username,
-}))(TopNav)
+export default connect(
+  state => ({
+    username: state.common.identity.username,
+  }),
+  dispatch => ({
+    logout: () => dispatch(clearIdentity()),
+  })
+)(TopNav)
